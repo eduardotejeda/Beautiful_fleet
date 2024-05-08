a7a5f8534f35bf87aaa77bf0e57232bc3fc1df75
Eduardo <eduardotejedadelgado@gmail.com>
2024-05-08 06:54:08 -0400

    Creado componente Usuario.js

--- server/CRUD/Usuario.js
+++ server/CRUD/Usuario.js
@@ -1,1 +1,9 @@
 import {supabase} from '../dataBase.js';
+
+/* 
+
+La tupla usuario contiene: id, created_at, nombre_usuario, emial y password.
+
+Inicio asumiendo que id y created_at se creand automaticamente.
+
+*/
